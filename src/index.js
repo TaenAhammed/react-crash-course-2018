4a98bda37c18e37699454884586f5ada683591ce
taen <taenahammed7@gmail.com>
2018-09-22 17:56:36 +0600

    My First Component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/Counter/Counter";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 registerServiceWorker();
