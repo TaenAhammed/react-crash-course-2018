b17e1b0206ffc873f5810e25c3c9fbc9d77a4015
taen <taenahammed7@gmail.com>
2018-09-22 18:05:38 +0600

    setting attrubutes and styles

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -4,11 +4,21 @@ class Counter extends Component {
         count: 0
     };
 
+    styles = {
+        fontSize: 40,
+        fontWeight: 700
+    };
+
     render() {
         return (
             <div className="container">
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span
+                    className="badge badge-primary m-2"
+                    style={{ fontSize: 40 }}
+                >
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
